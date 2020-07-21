85162ce70a8df107422f3e073882a9817e9443e7
Jordan-type <jordanmuthemba25@gmail.com>
2020-07-21 19:10:44 +0300

    ⚙ config file

---
+++ config.js
@@ -0,0 +1,24 @@
+    // replace the value below with the Telegram token you receive from @BotFather
+    var token = '1278996002:AAGgFzi0_3ioA9OPdUfDvNXtjOhp-5KTP50'
+        // firebase config from console.firebase
+        // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyDj12zzZ4B36x74iJKOAdue1fQyEORMHDQ",
+        authDomain: "air-bot-22aba.firebaseapp.com",
+        databaseURL: "https://air-bot-22aba.firebaseio.com",
+        projectId: "air-bot-22aba",
+        storageBucket: "air-bot-22aba.appspot.com",
+        messagingSenderId: "485118467309",
+        appId: "1:485118467309:web:7e40691ebc6648eae7c0e4",
+        measurementId: "G-EZJVYRVCVC"
+    };
+    // var config = {
+    //     apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    //     authDomain: "XXXXXXXXXXX.firebaseapp.com",
+    //     databaseURL: "https://XXXXXXXXX.firebaseio.com/",
+    //     projectId: "XXXXXXXX",
+    //     storageBucket: "XXXXXXXXXXXX.appspot.com",
+    // };
+
+    exports.token = token;
+    exports.firebase_config = firebaseConfig;
